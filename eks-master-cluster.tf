7a35bfaa27fdd7c3234e4cb71e83651bbc3938b7
Tushar K <tushars6016@gmail.com>
2022-10-11 15:07:17 +0530

    Update eks-master-cluster.tf

--- eks-master-cluster.tf
+++ eks-master-cluster.tf
@@ -11,15 +11,7 @@ resource "aws_vpc" "prod-vpc" {
 
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "prod-sub-1"
-  }
 
-}
 
 resource "aws_instance" "eks-master" {
   ami               = "ami-026b57f3c383c2eec"
